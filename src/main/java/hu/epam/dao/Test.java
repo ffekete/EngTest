4a756e1e251ce0c3a6859f48fbd531571432004a
Ferenc Fekete <Ferenc_Fekete@epam.com>
2016-07-14 09:53:21 +0200

    Test class developed

--- src/main/java/hu/epam/dao/Test.java
+++ src/main/java/hu/epam/dao/Test.java
@@ -1,8 +1,10 @@
 package hu.epam.dao;
 
-import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
@@ -14,11 +16,40 @@ public class Test {
 	}
 	
 	@Id
+	@GeneratedValue(strategy = GenerationType.TABLE)
 	private long id;
 	
 	public String text;
 	
-	public ArrayList<Answer> answers;
+	public List<Answer> answers;
 	
+	public Test(){
+		
+	}
+	
+	public Test(String text, List<Answer> answers){
+		this.text = text;
+		this.answers = answers;
+	}
+
+	public String getText() {
+		return text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
+
+	public List<Answer> getAnswers() {
+		return answers;
+	}
+
+	public void setAnswers(List<Answer> answers) {
+		this.answers = answers;
+	}
+
+	public long getId() {
+		return id;
+	}
 	
 }
